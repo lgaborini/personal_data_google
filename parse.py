98591bd4cff20243feee27cd0d1d6d08b26493dd
Lorenzo Gaborini <lorenzo.gaborini@unil.ch>
2017-09-29 10:52:13 +0200

    Re-structured with classes

--- parse.py
+++ parse.py
@@ -1,18 +1,60 @@
 from ingest import load_database, read_addresses
-import pandas as pd
+import pandas
 import re
 import tldextract
 
 
+class MainResult:
+    """The main GoogleResult associated to somebody"""
+
+    def __init__(self, siteName):
+        self.certified = False
+        self.candidates = []
+        self.certifiedResult = None
+        self.certifiedLink = None
+        self.siteName = siteName
+
+    def print(self):
+        print('  {0}? {1} ({2} candidates): {3} ->> {4}'.format(
+            self.siteName,
+            self.certified,
+            len(self.candidates),
+            self.certifiedResult.name,
+            self.certifiedLink))
+        try:
+            print('     {0}'.format(self.certifiedResult.description[:30]))
+        except AttributeError:
+            pass
+
+
+class LinkedInResult(MainResult):
+    def __init__(self):
+        super().__init__('LinkedIn')
+
+
+class ResearchGateResult(MainResult):
+    def __init__(self):
+        super().__init__('ResearchGate')
+
+
+class PersonalPageResult(MainResult):
+    def __init__(self):
+        super().__init__('personal page')
+
+
 def sanitize_string(s):
+    """Return the string, or a blank if invalid"""
     try:
         return s + ''
     except TypeError:
         return ''
 
 
+# -------------------
 # person-class functions
-def get_name(email, singleName=False):
+
+def get_name(email, single_string=False):
+    """Get dictionary of personal details from email"""
     df = read_addresses()
     row = df[df['EmailAddress'].values == email]
 
@@ -20,52 +62,58 @@ def get_name(email, singleName=False):
     last = sanitize_string(row['LastName'].values[0])
     company = sanitize_string(row['CompanyName'].values[0])
     country = sanitize_string(row['Country'].values[0])
-    if singleName:
+
+    if single_string:
         return first + ' ' + last
     else:
         return {'first': first, 'last': last, 'country': country, 'company': company}
 
 
-# is-class functions
+# result-class functions
 def is_LinkedinURL(result):
+    """A LinkedIn GoogleResult"""
     return 'linkedin.' in result.link
 
 
 def is_nameInTitle(result, name):
-    return [name['first'].lower() in result.name.lower() and 
-        name['last'].lower() in result.name.lower()]
+    """Somebody's name is mentioned in the result title"""
+    return [name['first'].lower() in result.name.lower() and
+            name['last'].lower() in result.name.lower()]
 
 
 # has-class functions
 def has_Linkedin(email, results):
+    """Get somebody's LinkedIn details"""
     candidates = []
     certified = False
     certifiedResult = None
     certifiedLink = None
 
     for r in results:
-        if is_LinkedinURL(r):
-            nameDict = get_name(email)
-            valid = is_nameInTitle(r, nameDict)
-            valid = valid and r'/pub/' not in r.link
-            valid = valid and nameDict['company'].lower() in r.description.lower()
+        if not is_LinkedinURL(r):
+            continue
 
-            if valid:
-                certified = True
-                certifiedResult = r
-                certifiedLink = r.link
+        nameDict = get_name(email)
+        valid = is_nameInTitle(r, nameDict)
+        valid = valid and r'/pub/' not in r.link
+        valid = valid and nameDict['company'].lower() in r.description.lower()
 
+        if valid:
+            certified = True
+            certifiedResult = r
+            certifiedLink = r.link
             candidates.append(r)
 
-    return {
-        'certified': certified,
-        'candidates': candidates,
-        'certifiedResult': certifiedResult,
-        'certifiedLink': certifiedLink
-    }
+    result = LinkedInResult()
+    result.certified = certified
+    result.candidates = candidates
+    result.certifiedResult = certifiedResult
+    result.certifiedLink = certifiedLink
+    return result
 
 
-def has_researchgate(email, results):
+def has_ResearchGate(email, results):
+    """Get somebody's ResearchGate details"""
     candidates = []
     certified = False
     certifiedResult = None
@@ -83,18 +131,18 @@ def has_researchgate(email, results):
             certified = True
             certifiedResult = r
             certifiedLink = r.link
+            candidates.append(r)
 
-        candidates.append(r)
-
-    return {
-        'certified': certified,
-        'candidates': candidates,
-        'certifiedResult': certifiedResult,
-        'certifiedLink': certifiedLink
-    }
+    result = ResearchGateResult()
+    result.certified = certified
+    result.candidates = candidates
+    result.certifiedResult = certifiedResult
+    result.certifiedLink = certifiedLink
+    return result
 
 
 def has_personal_page(email, results):
+    """Get somebody's personal page details"""
     candidates = []
     certified = False
     certifiedResult = None
@@ -109,17 +157,14 @@ def has_personal_page(email, results):
         # Forbidden domains
         if is_LinkedinURL(r):
             continue
-        if ('facebook' in r.link or
-            'twitter' in r.link or
-            'holaconnect' in r.link or
-            'crunchbase' in r.link or
-            'namenfinden' in r.link):
+        domains = ['facebook', 'twitter', 'holaconnect', 'crunchbase', 'namenfinden']
+        if any(d in r.link for d in domains):
             continue
 
         # Parse domains from email and link
         emailDomain = email.split('@')[-1]
         linkComponents = tldextract.extract(r.link)
-        
+
         # Personal page must contain his name/surname somewhere in the title
         nameDict = get_name(email)
 
@@ -142,54 +187,37 @@ def has_personal_page(email, results):
             certified = True
             certifiedResult = r
             certifiedLink = r.link
+            candidates.append(r)
 
-        candidates.append(r)
-
-    return {
-        'certified': certified,
-        'candidates': candidates,
-        'certifiedResult': certifiedResult,
-        'certifiedLink': certifiedLink
-    }
+    result = PersonalPageResult()
+    result.certified = certified
+    result.candidates = candidates
+    result.certifiedResult = certifiedResult
+    result.certifiedLink = certifiedLink
+    return result
 
 
 def print_Person(email, results, linkedin=False, personal=False, researchgate=False):
+    """Print someone's details"""
 
     nn = get_name(email)
 
     ll = has_Linkedin(email, results)
     pp = has_personal_page(email, results)
-    rg = has_researchgate(email, results)
+    rg = has_ResearchGate(email, results)
 
     print("*** {0} {1} @ {2} - {3} ***".format(nn['first'], nn['last'], nn['company'], email))
 
-    if linkedin and ll['certified']:
-        print('{0}: {1}'.format(email, get_name(email, singleName=True)))
-        print('  LINKEDIN? {0} ({1} candidates): {2} ->> {3}'.format(
-            ll['certified'],
-            len(ll['candidates']),
-            ll['certifiedResult'].name,
-            ll['certifiedResult'].link))
-        print('     {0}'.format(ll['certifiedResult'].description[:30]))
-
-    if personal and pp['certified']:
-        print('  PERSONAL? {0} ({1} candidates): {2} ->> {3}'.format(
-            pp['certified'],
-            len(pp['candidates']),
-            pp['certifiedResult'].name,
-            pp['certifiedResult'].link))
-        print('     {0}'.format(pp['certifiedResult'].description[:30]))
-
-    if researchgate and rg['certified']:
-        print('  RESEARCHGATE? {0} ({1} candidates): {2} ->> {3}'.format(
-            rg['certified'],
-            len(rg['candidates']),
-            rg['certifiedResult'].name,
-            rg['certifiedResult'].link))
-        print('     {0}'.format(rg['certifiedResult'].description[:30]))
+    if linkedin and ll.certified:
+        ll.print()
+    if personal and pp.certified:
+        pp.print()
+    if researchgate and rg.certified:
+        rg.print()
 
 
 def print_Results(results):
+    """Pretty print a list of GoogleResults"""
     for r in results:
         print('{0}: {1}'.format(r.index, r.link))
         print('   {0}'.format(r.name))
@@ -202,52 +230,50 @@ if __name__ == '__main__':
     emails = list(db.keys())
     emailsValid = [e for e in emails if db[e] is not None]
 
-    # Show summary stats
+    # Show and store summary stats
     summaries = []
     try:
         for e in emailsValid:
             results = db[e]
 
-            print_Person(e, results, personal=True)
-            nn = get_name(e)
+            print_Person(e, results, researchgate=True, personal=True)
+            name = get_name(e)
 
             # Linkedin
             ll = has_Linkedin(e, results)
-            if ll['certified']:
-                lr = ll['certifiedResult']
+            # lr = ll.certifiedResult
 
             # Personal pages
             pp = has_personal_page(e, results)
-            if pp['certified']:
-                lp = pp['certifiedResult']
+            # lp = pp.certifiedResult
 
-            # Researchgate
-            rg = has_researchgate(e, results)
-            if rg['certified']:
-                lr = rg['certifiedResult']
+            # ResearchGate
+            rg = has_ResearchGate(e, results)
+            # lr = rg.certifiedResult
 
+            # Summarize results to output file
             summary = {}
             summary['email'] = e
-            summary['firstName'] = nn['first']
-            summary['lastName'] = nn['last']
-            summary['company'] = nn['company']
+            summary['firstName'] = name['first']
+            summary['lastName'] = name['last']
+            summary['company'] = name['company']
 
-            summary['linkedinCertified'] = ll['certified']
-            summary['linkedinLink'] = ll['certifiedLink']
+            summary['linkedinCertified'] = ll.certified
+            summary['linkedinLink'] = ll.certifiedLink
 
-            summary['personalCertified'] = pp['certified']
-            summary['personalLink'] = pp['certifiedLink']
+            summary['personalCertified'] = pp.certified
+            summary['personalLink'] = pp.certifiedLink
 
-            summary['researchgateCertified'] = rg['certified']
-            summary['researchgateLink'] = rg['certifiedLink']
+            summary['researchgateCertified'] = rg.certified
+            summary['researchgateLink'] = rg.certifiedLink
             summaries.append(summary)
 
         # Export dataframe
         print('Exporting dataframe...')
-        df_summaries = pd.DataFrame(summaries)
+        df_summaries = pandas.DataFrame(summaries)
         df_summaries.to_csv('results.csv', sep=';', index=False)
         print('Done.')
 
     except KeyboardInterrupt:
         print('')
-        print("*** Aborting (interrupt) ***")
+        print('*** Aborting (interrupt) ***')
